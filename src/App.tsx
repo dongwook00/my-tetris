accde54a9149a8aa2842f9bda4a9da4e89624591
Dongwook Kim <dongwookkim@nkia.co.kr>
2021-09-18 12:05:54 +0900

    add  helper: tetromino generation

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import { StyledTetrisWrapper, StyledTetris } from './App.styles';
 
 const App: React.FC = () => {
-  return <h1>Hello World</h1>;
+  return (
+    <StyledTetrisWrapper role='button' tabIndex={0}>
+      <StyledTetris>Start</StyledTetris>
+    </StyledTetrisWrapper>
+  );
 };
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createGlobalStyle } from 'styled-components';
 import App from './App';
 
+const GlobalStyles = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    background: #fff;
+  }
+`;
+
 ReactDOM.render(
-  <React.StrictMode>
+  <>
+    <GlobalStyles />
     <App />
-  </React.StrictMode>,
+  </>,
   document.getElementById('root'),
 );
